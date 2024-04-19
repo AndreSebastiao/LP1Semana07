307d201e610b9dc62d2124c884de0935bc110584
AndreSebastiao <andresebastiao755@gmail.com>
2024-04-19 22:59:53 +0100

    Ex 5

---
+++ GameUnits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GameUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit mu = new MilitaryUnit(1, 10, 3);
+            Unit su = new SettlerUnit();
+
+            MilitaryUnit attackUnit = mu as MilitaryUnit;
+
+            attackUnit.Move();
+            su.Move();
+
+            Console.WriteLine($"Military health {attackUnit.Health}");
+            Console.WriteLine($"Settler Health {su.Health}");
+            Console.WriteLine($"Cost of Military {attackUnit.Cost}");
+            Console.WriteLine($"Cost of Settler {su.Cost}");
+
+            attackUnit.Attack(su);
+            Console.WriteLine($"Settler Health after: {su.Health}");
+        }
+    }
+}
